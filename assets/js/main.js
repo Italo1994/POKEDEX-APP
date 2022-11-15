ae1787a2ec0a39303bc5bd836021e2519643467d
Italo1994 <italohortiz2015@gmail.com>
2022-11-15 17:01:36 -0300

    Recebendo os dados da API e organizando a resposta com o fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,16 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then( function(response) {
-    console.log(response);
-} )
+fetch(url)
+        .then( (response) => {
+            return response.json();
+        })
+        .then( (jsonBody) => {
+            console.log(jsonBody);
+        })
+        .catch( (error) => {
+            console.log(error);
+        })
+        .finally( () => {
+            console.log("Requisição concluída!");
+        })
