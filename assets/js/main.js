b4e0fd865d9ecc377ca71b6bcb28546c534ae5c5
Italo1994 <italohortiz2015@gmail.com>
2022-11-16 00:11:01 -0300

    Transformando a lista de pokemons recebidos via API em novos elementos de lista HTML atraves da criação da função convertPokemonToList. Alterando o padding da classe content no CSS global.css de 1rem para 1.225rem

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,35 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToList(pokemon) {
+    return `
+    <li class="pokemon">
+        <!-- k4ng0gh -->
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" />
+        </div>
+    </li>
+    `;
+}
+
+const pokemonOl = document.getElementById("pokemonList");
+
 fetch(url)
-        .then( (response) => {
-            return response.json();
+        .then( (response) => response.json())
+        .then( (jsonBody) => jsonBody.results)
+        .then( (pokemonList) => {
+            for(let i=0; i<pokemonList.length; i++) {
+                const pokemon = pokemonList[i];
+                
+                pokemonOl.innerHTML += convertPokemonToList(pokemon);
+            }
         })
-        .then( (jsonBody) => {
-            console.log(jsonBody);
-        })
-        .catch( (error) => {
-            console.log(error);
-        })
-        .finally( () => {
-            console.log("Requisição concluída!");
-        })
+        .catch( (error) => console.log(error))
